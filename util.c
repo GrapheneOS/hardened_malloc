1fbf0e27f594105816cc374fc587575cbc1066c8
Daniel Micay <danielmicay@gmail.com>
2018-10-03 16:55:25 -0400

    make error reporting more robust

--- util.c
+++ util.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -5,10 +6,26 @@
 
 #include "util.h"
 
+static int write_full(int fd, const char *buf, size_t length) {
+    do {
+        ssize_t bytes_written = write(fd, buf, length);
+        if (bytes_written == -1) {
+            if (errno == EINTR) {
+                continue;
+            }
+            return -1;
+        }
+        buf += bytes_written;
+        length -= bytes_written;
+    } while (length);
+
+    return 0;
+}
+
 COLD noreturn void fatal_error(const char *s) {
     const char *prefix = "fatal allocator error: ";
-    write(STDERR_FILENO, prefix, strlen(prefix));
-    write(STDERR_FILENO, s, strlen(s));
-    write(STDERR_FILENO, "\n", 1);
+    (void)(write_full(STDERR_FILENO, prefix, strlen(prefix)) != -1 &&
+        write_full(STDERR_FILENO, s, strlen(s)) != -1 &&
+        write_full(STDERR_FILENO, "\n", 1));
     abort();
 }
