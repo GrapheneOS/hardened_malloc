59def67979d980dd91d5cc5b1287ab3856e2044c
Daniel Micay <danielmicay@gmail.com>
2018-10-23 07:40:14 -0400

    add inlined check for -1 with pkey_set

--- malloc.c
+++ malloc.c
@@ -94,15 +94,24 @@ int get_metadata_key(void) {
 #endif
 }
 
+#ifdef USE_PKEY
+static inline void thread_set_metadata_access(unsigned access) {
+    if (ro.metadata_pkey == -1) {
+        return;
+    }
+    pkey_set(ro.metadata_pkey, access);
+}
+#endif
+
 static inline void thread_unseal_metadata(void) {
 #ifdef USE_PKEY
-    pkey_set(ro.metadata_pkey, 0);
+    thread_set_metadata_access(0);
 #endif
 }
 
 static inline void thread_seal_metadata(void) {
 #ifdef USE_PKEY
-    pkey_set(ro.metadata_pkey, PKEY_DISABLE_ACCESS);
+    thread_set_metadata_access(PKEY_DISABLE_ACCESS);
 #endif
 }
 
