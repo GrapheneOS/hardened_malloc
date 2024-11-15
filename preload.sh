5c87c6bb2db55f1b2ce14bbd4b2318ffe963265a
AlbydS <119180144+AlbydST@users.noreply.github.com>
2024-11-15 18:48:40 +0000

    Remove automatic building

--- preload.sh
+++ preload.sh
@@ -2,7 +2,6 @@
 
 # preload.sh - Test dynamically linked executables
 
-[ ! -f "out/libhardened_malloc.so" ] && make -j"$(nproc)" # If the library isn't found, build it.
 [ -n "${LD_PRELOAD}" ] && LD_PRELOAD="${LD_PRELOAD} " # If LD_PRELOAD is already set, add a space.
 export LD_PRELOAD="${LD_PRELOAD}${PWD}/out/libhardened_malloc.so" # Add the library to LD_PRELOAD.
 exec "$@" # Run the command.
