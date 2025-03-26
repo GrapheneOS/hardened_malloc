1d7fc7ffe0f8068a32ab2ba33df7515e7bad3f5f
Christian Göttsche <cgzones@googlemail.com>
2025-03-26 10:21:16 +0100

    support GCC15
    GCC 15 starts warning about non NUL-terminated string literals:
    
        chacha.c:44:31: error: initializer-string for array of ‘char’ truncates NUL terminator but destination lacks ‘nonstring’ attribute (17 chars into 16 available) [-Werror=unterminated-string-initialization]
           44 | static const char sigma[16] = "expand 32-byte k";
              |                               ^~~~~~~~~~~~~~~~~~

--- chacha.c
+++ chacha.c
@@ -41,7 +41,7 @@ static const unsigned rounds = 8;
     a = PLUS(a, b); d = ROTATE(XOR(d, a), 8); \
     c = PLUS(c, d); b = ROTATE(XOR(b, c), 7);
 
-static const char sigma[16] = "expand 32-byte k";
+static const char sigma[16] NONSTRING = "expand 32-byte k";
 
 void chacha_keysetup(chacha_ctx *x, const u8 *k) {
     x->input[0] = U8TO32_LITTLE(sigma + 0);
