17fd44ab55dc4bbb88558dde0d55eb1410e89066
rdevshp <rdevshp@gmail.com>
2026-04-24 23:19:56 +0800

    add proper declaration for h_mallinfo2 when H_MALLOC_PREFIX and __GLIBC__ are both defined

--- include/h_malloc.h
+++ include/h_malloc.h
@@ -72,6 +72,9 @@ void h_malloc_stats(void);
 #if defined(__GLIBC__) || defined(__ANDROID__)
 struct mallinfo h_mallinfo(void);
 #endif
+#if defined(__GLIBC__)
+struct mallinfo2 h_mallinfo2(void);
+#endif
 #ifndef __ANDROID__
 int h_malloc_info(int options, FILE *fp);
 #endif
