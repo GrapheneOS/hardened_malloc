f2feaa86047353247755046f3b4edd3a3657b9ff
Daniel Micay <daniel.micay@grapheneos.org>
2026-09-04 15:45:48 -0400

    always set VMA name for quarantined large allocation

--- h_malloc.c
+++ h_malloc.c
@@ -1021,9 +1021,8 @@ static void regions_quarantine_deallocate_pages(void *p, size_t size, size_t gua
 
     if (unlikely(memory_map_fixed(p, size))) {
         memory_purge(p, size);
-    } else {
-        memory_set_name(p, size, "malloc large quarantine");
     }
+    memory_set_name(p, size, "malloc large quarantine");
 
     struct quarantine_info target =
         (struct quarantine_info){(char *)p - guard_size, size + guard_size * 2};
